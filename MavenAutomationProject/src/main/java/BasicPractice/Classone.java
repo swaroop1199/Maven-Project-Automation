1faa04feb6e66dccf53484c532c5324dc89911f6
swaroop1199 <hiremathswaroop@gmail.com>
2024-05-10 10:54:36 +0530

    practice commit

---
+++ MavenAutomationProject/src/main/java/BasicPractice/Classone.java
@@ -0,0 +1,13 @@
+package BasicPractice;
+
+public class Classone {
+
+	public static void main(String[] args) {
+		
+		Classtwo t1 = new Classtwo();
+		
+		t1.display();
+
+	}
+
+}
---
+++ MavenAutomationProject/src/main/java/BasicPractice/Classtwo.java
@@ -0,0 +1,9 @@
+package BasicPractice;
+
+public class Classtwo {
+
+	public void display(){
+		System.out.println("This is display method");
+	}
+	
+}
